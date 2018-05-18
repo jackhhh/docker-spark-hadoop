650c274b0efe3c88334ac193c29326f8a810b9ea
Jack Huang <minjie.h@outlook.com>
2018-05-18 12:06:25 +0800

    exec hadoop startup shell in Source way

--- master/master.sh
+++ master/master.sh
@@ -2,6 +2,18 @@
 
 # master startup
 
+# hadoop startup
+
+. hadoop namenode -format
+. hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode
+. hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
+. yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager
+. yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager
+. mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver
+
+
+# spark startup
+
 export SPARK_MASTER_HOST=`spark-master`
 
 . "/usr/local/spark-2.3.0-bin-hadoop2.7/sbin/spark-config.sh"

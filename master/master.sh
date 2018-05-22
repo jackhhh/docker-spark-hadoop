cea15567c8f0b59b0410f44e35def87085495b4f
jackhhh <minjie.h@outlook.com>
2018-05-22 10:24:42 +0800

    add Y

--- master/master.sh
+++ master/master.sh
@@ -4,7 +4,7 @@
 
 # hadoop startup
 
-hadoop namenode -format
+echo y | hadoop namenode -format
 hadoop-daemon.sh start namenode
 hadoop-daemon.sh start datanode
 yarn-daemon.sh start resourcemanager
